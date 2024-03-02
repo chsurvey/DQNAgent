866789828b5b150596b5beb837d949cfc3d9a951
taegukang35 <67684178+taegukang35@users.noreply.github.com>
2024-03-03 03:29:45 +0900

    Update trainDQN.py

--- SSBM/trainDQN.py
+++ SSBM/trainDQN.py
@@ -24,6 +24,8 @@ parser.add_argument("--iso", default="/home/vlab/SSBM/ssbm.iso", type=str,
 args = parser.parse_args()
 
 agent = DQNAgent()
+#agent.policy_net = torch.load("/home/vlab/SSBM/melee-env/model_180.pth", map_location=agent.device)
+#agent.target_net = torch.load("/home/vlab/SSBM/melee-env/model_180.pth", map_location=agent.device)
 
 players = [agent, CPU(enums.Character.FOX, 9)] #CPU(enums.Character.KIRBY, 5)]
 env = MeleeEnv(args.iso, players, fast_forward=True, ai_starts_game=True)
@@ -46,6 +48,8 @@ for episode in range(episodes):
           
         next_gamestate, done = env.step()
         obs, reward, done, info = agent.observation_space(next_gamestate)
+        #if abs(reward) > 0.1:
+        #    print(reward)
         total_reward += reward
         
         reward = torch.tensor([reward], device=device)
