e83ffde6cb3f110ac94591c67fbc1e4717462d9b
chsurvey <58624692+chsurvey@users.noreply.github.com>
2024-03-10 04:04:41 +0900

    Update trainDQN.py
    freq마다 env를 재시작

--- SSBM/melee-env/trainDQN.py
+++ SSBM/melee-env/trainDQN.py
@@ -3,6 +3,7 @@ from melee_env.env import MeleeEnv
 from melee_env.agents.basic import *
 from melee_env.agents.util import *
 
+import time
 import math
 import random
 import matplotlib
@@ -15,67 +16,83 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+from tqdm import tqdm
+import psutil
+import sys
+
 from DQNAgent import DQNAgent
-from NstepDQNAgent import NstepDQNAgent
 
 parser = argparse.ArgumentParser(description="Example melee-env demonstration.")
-parser.add_argument("--iso", default="/home/vlab/SSBM/ssbm.iso", type=str, 
+parser.add_argument("--iso", default="./ssbm.iso", type=str, 
     help="Path to your NTSC 1.02/PAL SSBM Melee ISO")
 
 args = parser.parse_args()
 
-agent = NstepDQNAgent(n_step=60) #DQNAgent()
-#agent.policy_net = torch.load("/home/vlab/SSBM/melee-env/model_180.pth", map_location=agent.device)
-#agent.target_net = torch.load("/home/vlab/SSBM/melee-env/model_180.pth", map_location=agent.device)
+agent = DQNAgent()
 
-players = [agent, CPU(enums.Character.FOX, 9)] #CPU(enums.Character.KIRBY, 5)]
+players = [agent, CPU(enums.Character.FOX, 1)] #CPU(enums.Character.KIRBY, 5)]
 env = MeleeEnv(args.iso, players, fast_forward=True, ai_starts_game=True)
 device = agent.device #torch.device("cuda" if torch.cuda.is_available() else "cpu")
             
-episodes = 1000
+episodes = 100
 episode_list = []
 reward_list = []
+
+freq = 3
+save_name = time.strftime("%y-%m-%d_%H-%M-%S", time.localtime())
+
 env.start()
-
-t = 0
-
-for episode in range(episodes):
-    gamestate, done = env.setup(enums.Stage.BATTLEFIELD)
-    total_reward = 0
-    
-    while not done:
-        action = agent.select_action(gamestate)
-        # print(action)
-        players[0].apply(action)
-        players[1].act(gamestate)
-          
-        next_gamestate, done = env.step()
-        obs, reward, done, info = agent.observation_space(next_gamestate)
+for lvl in range(1,10):
+    env.players[1].lvl = lvl
+    for episode in tqdm(range(episodes)):
+        if episode % freq == freq-1:
+            for proc in psutil.process_iter():
+                if proc.name() == "Slippi Dolphin.exe":
+                    parent_pid = proc.pid
+                    parent = psutil.Process(parent_pid)
+                    for child in parent.children(recursive=True):
+                        child.kill()
+                    parent.kill()
+            players = [agent, CPU(enums.Character.FOX, lvl)] #CPU(enums.Character.KIRBY, 5)]
+            env = MeleeEnv(args.iso, players, fast_forward=True, ai_starts_game=True)
+            # agent.policy_net.load_state_dict(torch.load(save_name + ".pth"))
+            # agent.target_net.load_state_dict(agent.policy_net.state_dict())
+            env.start()
         
-        if done == True:
-            reward = -5 # die
+        print("==>setting env...")
+        gamestate, done = env.setup(enums.Stage.FINAL_DESTINATION)
+        print("done.")
+        total_reward = 0
+        
+        while not done:
+            action = agent.select_action(gamestate)
+            # print(action)
+            players[0].apply(action)
+            players[1].act(gamestate)       
             
-        total_reward += reward
-        reward = torch.tensor([reward], device=device)
-        d = torch.tensor([1 if done else 0], device=device)
-        state = agent.convert_state(gamestate)
-        next_state = agent.convert_state(next_gamestate)
-        agent.update(state, action, reward, next_state, d)
+            next_gamestate, done = env.step()
+            obs, reward, done, info = agent.observation_space(next_gamestate)
+            total_reward += reward
+            
+            reward = torch.tensor([reward], device=device)
+            done = torch.tensor([1 if done else 0], device=device)
+            state = agent.convert_state(gamestate)
+            next_state = agent.convert_state(next_gamestate)
+
+            agent.update(state, action, reward, next_state, done)
+            gamestate = next_gamestate
+            #print(len(agent.replay_buffer))
         
-        if abs(reward) == 5:
-            agent.nstep_buffer.clear()
-            episode_list.append(t + 1)
-            reward_list.append(total_reward)
+        episode_list.append(episode + 1)
+        reward_list.append(total_reward)
+        
+        if episode % 30 == 29:
+            torch.save(agent.policy_net, f'./model/DQN_{episode+1}.pth')
+            print(reward_list[-1])
+            plt.figure()
             plt.plot(episode_list, reward_list, color='blue')
             plt.xlabel('Episode')
             plt.ylabel('Total Reward')
-            plt.pause(0.05)
-            t += 1
-            total_reward = 0
-        
-        gamestate = next_gamestate
-    
-    if episode % 30 == 0:
-        torch.save(agent.policy_net, f'/home/vlab/SSBM/nstepDQN/{agent.n_step}step_{episode}.pth')
-        print(reward_list[-1])
-        plt.savefig(f'/home/vlab/SSBM/nstepDQN/{agent.n_step}step.png')
+            plt.savefig(f'reward_plot.png')
+            plt.close()
+    print("episode",episode+1,"ended.")
